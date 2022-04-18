e5d78f0a94b45c7076fd2f2d8eafb689e903310a
FlutLab.io <>
2022-04-18 00:56:26 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class MyHomePage extends StatelessWidget {
                   ListTile(
                     leading: ClipRRect(
                       borderRadius: BorderRadius.circular(5.0),
-                      child: Image.network("https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80", width: 30.0, height: 40.0),
+                      child: Image.network("https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80", width: 50.0, height: 60.0),
                     ),
                     title: Text("Brand New Shoes", maxLines: 1, overflow: TextOverflow.ellipsis),
                     subtitle: Text("SH-231", maxLines: 1, overflow: TextOverflow.ellipsis),

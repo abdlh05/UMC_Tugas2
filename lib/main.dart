405adfeda9b5f7aec1aacecb43c4a2aa3ac053e5
FlutLab.io <>
2022-04-18 00:05:45 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -39,26 +39,28 @@ class MyHomePage extends StatelessWidget {
           itemCount: 3,
           itemBuilder: (context, index) {
             return Padding(
-              padding: EdgeInsets.all(10.0),
+              padding: EdgeInsets.all(5.0),
               child: Card(
                 shape: BeveledRectangleBorder(
                   borderRadius: BorderRadius.circular(5.0),
                 ),
                 color: Colors.blueGrey[50],
-                child: Padding(
-                  padding: EdgeInsets.only(top: 2.0, bottom: 2.0),
-                  child: ListTile(
+                child: Column(
+                  : EdgeInsets.only(top: 2.0, bottom: 2.0),padding
+                  children: <Widget>[ 
+                  ListTile(
                     leading: ClipRRect(
                       borderRadius: BorderRadius.circular(5.0),
                       child: Image.network("https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80"),
                     ),
                     title: Text("Brand New Shoes", maxLines: 1, overflow: TextOverflow.ellipsis),
                     subtitle: Text("SH-231", maxLines: 1, overflow: TextOverflow.ellipsis),
-                    trailing: Icon(
-                      Icons.delete_forever_rounded,
-                    ),
-                    isThreeLine: true,
+                    trailing: Icon(Icons.delete_forever_rounded,),isThreeLine: true,
+                    
                   ),
+
+                  ]
+                  
                 ),
               ),
             );

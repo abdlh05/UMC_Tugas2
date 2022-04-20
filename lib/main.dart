4d9443168c38eafc8978cfec96518bfe124d46bd
FlutLab.io <>
2022-04-20 07:53:31 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,11 @@ class MyHomePage extends StatelessWidget {
                         fit: BoxFit.fill,
                         child: ClipRRect(
                           borderRadius: BorderRadius.circular(5.0),
-                          child: Image.network("https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80"),
+                          child: Align(
+                            alignment: Alignment.center,
+                            heightFactor: 0.5,
+                            child: Image.network(""),
+                          ),
                         )),
                     title: Column(children: [
                       Text("Brand New Shoes", maxLines: 1, overflow: TextOverflow.ellipsis),

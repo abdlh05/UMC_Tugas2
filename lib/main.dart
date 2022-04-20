6491f681836d3cc5010b1ca0597402631dc8b68b
FlutLab.io <>
2022-04-20 08:54:18 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
       body: ListView.builder(
-          padding: EdgeInsets.only(top: 30.0, right: 8.0, left: 8.0),
+          margin: EdgeInsets.only(top: 30.0, right: 8.0, left: 8.0),
           itemCount: 3,
           itemBuilder: (context, index) {
             return Card(

8e0b09543a224625acf214a059ff3f8aa2bbe9d0
FlutLab.io <>
2022-04-20 09:04:16 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,7 @@ class MyHomePage extends StatelessWidget {
                     )
                   ]),
                   subtitle: Container(
+                    width: MediaQuery.of(context).size.width * 0.5,
                     margin: const EdgeInsets.only(top: 10),
                     decoration: BoxDecoration(
                         color: Colors.blue,

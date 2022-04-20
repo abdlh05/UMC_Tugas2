48c776fd1884dc3f5acf39ab27209474af09f359
FlutLab.io <>
2022-04-20 06:46:26 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -61,48 +61,6 @@ class CustomListItem extends StatelessWidget {
   }
 }
 
-class _ProductList extends StatelessWidget {
-  const _ProductList({
-    Key? key,
-    required this.title,
-    required this.user,
-    required this.stock,
-  }) : super(key: key);
-
-  final String title;
-  final String user;
-  final Widget stock;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(5.0, 0.0, 0.0, 0.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: const TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 14.0,
-            ),
-          ),
-          const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
-          Text(
-            user,
-            style: const TextStyle(fontSize: 10.0),
-          ),
-          const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
-          Text(
-            '$stock views',
-            style: const TextStyle(fontSize: 10.0),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class MyHomePage extends StatelessWidget {
   final String title;
 
@@ -123,56 +81,38 @@ class MyHomePage extends StatelessWidget {
           icon: Icon(Icons.shopping_bag_outlined, color: Colors.blue),
         ),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
-        itemExtent: 106.0,
-        children: <CustomListItem>[
-          CustomListItem(
-            user: 'Flutter',
-            stock: Container(decoration: BoxDecoration()),
-            thumbnail: Container(
-              decoration: const BoxDecoration(color: Colors.blue),
-            ),
-            title: 'The Flutter YouTube Channel',
-          ),
-          CustomListItem(
-            user: 'Dash',
-            stock: Container(decoration: BoxDecoration()),
-            thumbnail: Container(
-              decoration: const BoxDecoration(color: Colors.yellow),
-            ),
-            title: 'Announcing Flutter 1.0',
-          ),
-        ],
-      ),
-      // body: ListView.builder(
-      //     padding: EdgeInsets.only(top: 20.0),
-      //     itemCount: 3,
-      //     itemBuilder: (context, index) {
-      //       return Padding(
-      //         padding: EdgeInsets.all(5.0),
-      //         child: Card(
-      //           shape: BeveledRectangleBorder(
-      //             borderRadius: BorderRadius.circular(2.0),
-      //           ),
-      //           color: Colors.blueGrey[50],
-      //           child: Column(children: <Widget>[
-      //             ListTile(
-      //               leading: ClipRRect(
-      //                 borderRadius: BorderRadius.circular(5.0),
-      //                 child: Image.network("https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80", width: 50.0, height: 60.0),
-      //               ),
-      //               title: Text("Brand New Shoes", maxLines: 1, overflow: TextOverflow.ellipsis),
-      //               subtitle: Text("SH-231", maxLines: 1, overflow: TextOverflow.ellipsis),
-      //               trailing: Icon(
-      //                 Icons.delete_forever_rounded,
-      //               ),
-      //               isThreeLine: true,
-      //             ),
-      //           ]),
-      //         ),
-      //       );
-      //     }),
+      
+      body: ListView.builder(
+          padding: EdgeInsets.only(top: 20.0),
+          itemCount: 3,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Card(
+                shape: BeveledRectangleBorder(
+                  borderRadius: BorderRadius.circular(2.0),
+                ),
+                color: Colors.blueGrey[50],
+                child: Column(children: <Widget>[
+                  ListTile(
+                    leading: ClipRRect(
+                      borderRadius: BorderRadius.circular(5.0),
+                      child: Image.network("https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80", width: 50.0, height: 60.0),
+                    ),
+                    title: Column(children:[
+                      Text("Brand New Shoes", maxLines: 1, overflow: TextOverflow.ellipsis),
+                      Text("SH-231", maxLines: 1, overflow: TextOverflow.ellipsis),
+                      ]),
+                    subtitle: 
+                    trailing: Icon(
+                      Icons.delete_forever_rounded,
+                    ),
+                    isThreeLine: true,
+                  ),
+                ]),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton.extended(
         label: Text('New Product'),
         icon: Icon(Icons.shopping_bag_rounded),

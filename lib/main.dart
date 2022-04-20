88d92e9e410bd107d5e8c5aa2b07307d6f08b525
FlutLab.io <>
2022-04-20 09:17:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -67,12 +67,12 @@ class MyHomePage extends StatelessWidget {
                       child: Text("NK-231", style: TextStyle(color: Colors.grey, fontSize: 10.0)),
                     )
                   ]),
-                  subtitle: Container(
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                      ),
-                      child: Align(alignment: Alignment.center, child: Text("Stock 1"))),
+                  subtitle: IntrinsicWidth(
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                          ),
+                          child: Align(alignment: Alignment.center, child: Text("Stock 1")))),
                   trailing: Icon(
                     Icons.delete_forever_rounded,
                   ),

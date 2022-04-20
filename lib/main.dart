bc3dede43f2c200412a5b2fb557857e0c302435b
FlutLab.io <>
2022-04-20 08:52:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -42,53 +42,50 @@ class MyHomePage extends StatelessWidget {
           padding: EdgeInsets.only(top: 30.0, right: 8.0, left: 8.0),
           itemCount: 3,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.only(top: 10.0),
-              child: Card(
-                shape: BeveledRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                color: Colors.blueGrey[50],
-                child: Column(children: <Widget>[
-                  ListTile(
-                    leading: ClipRRect(
-                      borderRadius: BorderRadius.circular(10.0),
-                      child: Image.network(
-                        "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80",
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    title: Column(children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text("Brand New Shoes", maxLines: 1, overflow: TextOverflow.ellipsis),
-                      ),
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: Text("NK-231", style: TextStyle(color: Colors.grey, fontSize: 10.0)),
-                      )
-                    ]),
-                    subtitle: Container(
-                      margin: const EdgeInsets.only(top: 10),
-                      decoration: BoxDecoration(
-                          color: Colors.blue,
-                          border: Border.all(
-                            color: Colors.transparent,
-                            width: 0,
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(50))),
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Text('Hello, Flutter!', style: TextStyle(fontSize: 16, color: Colors.white)),
-                      ),
-                    ),
-                    trailing: Icon(
-                      Icons.delete_forever_rounded,
-                    ),
-                    isThreeLine: true,
-                  ),
-                ]),
+            return Card(
+              shape: BeveledRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
               ),
+              color: Colors.blueGrey[50],
+              child: Column(children: <Widget>[
+                ListTile(
+                  leading: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Image.network(
+                      "https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bmlrZSUyMHNob2V8ZW58MHx8MHx8&w=1000&q=80",
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  title: Column(children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text("Brand New Shoes", maxLines: 1, overflow: TextOverflow.ellipsis),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text("NK-231", style: TextStyle(color: Colors.grey, fontSize: 10.0)),
+                    )
+                  ]),
+                  subtitle: Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.blue,
+                        border: Border.all(
+                          color: Colors.transparent,
+                          width: 0,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: Text('Hello, Flutter!', style: TextStyle(fontSize: 16, color: Colors.white)),
+                    ),
+                  ),
+                  trailing: Icon(
+                    Icons.delete_forever_rounded,
+                  ),
+                  isThreeLine: true,
+                ),
+              ]),
             );
           }),
       floatingActionButton: FloatingActionButton.extended(
